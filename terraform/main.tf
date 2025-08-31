3582c73d6ed33d9f0f7ecb287927f9a2aa2011a9
Haripriya2408 <haripriyaveluchamy@gmail.com>
2025-08-31 15:59:32 +0530

    corrected version

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,3 @@
-# define aws terraform prvider
-
-terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~>5.0"
-            }
-    }
-    required_version = ">=1.6.0"
-}
-
-provider "aws" {
-  region = var.region
-}
-
 # define VPC in aws
 
 resource "aws_vpc" "main" {
