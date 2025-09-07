3594c52bcbb41fc9234f0792b921db92cf79b4d3
Haripriya2408 <haripriyaveluchamy@gmail.com>
2025-09-07 23:28:15 +0530

    corrected yml

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -7,7 +7,7 @@ resource "aws_cloudwatch_log_group" "ecs_logs" {
   retention_in_days = 14
 }
 
-# Task Definitions
+# Task Definition
 resource "aws_ecs_task_definition" "app" {
   family                   = "${var.project_name}-task"
   network_mode             = "awsvpc"
